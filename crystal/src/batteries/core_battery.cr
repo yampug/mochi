01c9c190465ad98d46668700e7d46659cc2e3398
yampug <bpsekii@gmail.com>
2025-10-03 08:30:10 +0200

    add detection for orion and firefox

--- crystal/src/batteries/core_battery.cr
+++ crystal/src/batteries/core_battery.cr
@@ -456,16 +456,36 @@ class CoreBattery
           return vendor === "Apple Computer, Inc."
         end
 
+        def self.is_orion(user_agent_data)
+          if `#{user_agent_data}`
+            return `#{user_agent_data}.brands[0].brand === "Orion"`
+          end
+          return false
+        end
+
+        def self.is_firefox(user_agent)
+          return user_agent.include?("Firefox")
+        end
+
         def self.identify
           user_agent = `window.clientInformation.userAgent`
           vendor = `window.clientInformation.vendor`
+          user_agent_data = `window.clientInformation.userAgentData`
 
           if is_chrome_based(vendor)
             return "chrome_based"
           end
 
+          if is_orion(user_agent_data)
+            return "orion"
+          end
+
           if is_safari(vendor)
             return "safari"
+          end
+
+          if is_firefox(user_agent)
+            return "firefox"
           end
 
           return "unknown"
