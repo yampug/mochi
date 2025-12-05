cb0dd5491f5ec7980a07631791ba5015316a2631
yampug <bpsekii@gmail.com>
2025-12-05 20:59:06 +0100

    back button for dev test pages

---
+++ devground/components/lib/a/dev_root_back_button.rb
@@ -0,0 +1,39 @@
+# typed: true
+class DevRootBackButton
+
+  @tag_name = "dev-root-back-button"
+
+  def initialize
+  end
+
+  def reactables
+    []
+  end
+
+  def html
+    %Q{
+      <a href="/" data-external style="margin-right: 10px;">&lt; Go to Root</a>
+    }
+  end
+
+  def css
+    %Q{
+        a {
+          background: #cf6d6d;
+          padding: 10px;
+          border-radius: 12px;
+          position: absolute;
+          right: 10px;
+          top: 10px;
+          color: white;
+          text-decoration: none;
+        }
+      }
+  end
+
+  def mounted
+  end
+
+  def unmounted
+  end
+end
