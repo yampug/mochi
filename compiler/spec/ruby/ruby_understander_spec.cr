c28c6f0b1cca5ff00612166959734ce901534090
yampug <bpsekii@gmail.com>
2025-12-21 00:08:56 +0100

    Write tests for class name extraction

--- compiler/spec/ruby/ruby_understander_spec.cr
+++ compiler/spec/ruby/ruby_understander_spec.cr
@@ -2,11 +2,45 @@ require "spec"
 require "../spec_data_loader"
 require "../../src/ruby/ruby_understander"
 
+def check_class_name(exp_name : String, code : String)
+  name = RubyUnderstander.class_name(code)
+  name.should eq(exp_name)
+end
+
 describe RubyUnderstander do
+
   it "processes simple if condition" do
     code = SpecDataLoader.load("ruby/a_layout.rb")
     name = RubyUnderstander.class_name(code)
     name.should eq("ALayout")
+  end
+
+  it "class_name: inheritance" do
+    check_class_name("MyClass", "class MyClass < ParentClass ")
+  end
+
+  it "class_name: namespacing" do
+    check_class_name("Class", "class My::Nested::Class")
+  end
+
+  it "class_name: extra spaces" do
+    check_class_name("MyClass", "class   MyClass   ;   end")
+  end
+
+  it "class_name: comments" do
+    check_class_name("MyClass", "class MyClass # comment")
+  end
+
+  it "class_name: embedded string" do
+    check_class_name("", "\"class NotAClass\"")
+  end
+
+  it "class_name: class in a comment" do
+    check_class_name("", "# class InComment ")
+  end
+
+  it "class_name: nested class" do
+    check_class_name("A", "class A; class B; end; end")
   end
 
 end
